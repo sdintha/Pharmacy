e8a3234a763940d68df983318775c46294f53d0c
DevvOps <devops@localhost.localdomain>
2019-11-11 07:45:14 -0800

    first commit

---
+++ src/main/java/DBIntializer.java
@@ -0,0 +1,7 @@
+
+public interface DBIntializer {
+String DRIVER="com.mysql.jdbc.Driver";
+String CON_STRING="jdbc:mysql://localhost/pharmacyDB";
+String USERNAME="pharmacy";
+String PASSWORD="pharmacy";
+}
